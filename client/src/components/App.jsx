f3d476c8cf5b17cc28160c4c09184459eacfe3b5
Stephen Hyde <shh2112@gmail.com>
2021-03-30 15:50:13 -0400

    create navbar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,31 +5,24 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import Navbar from './Navbar';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="container">
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
   )
 };
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/"
+        className="navbar-brand" >
+        Home
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
