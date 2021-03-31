3c8046aa67d67d2a6c8fba2f247706f7396eef86
Stephen Hyde <shh2112@gmail.com>
2021-03-31 10:05:25 -0400

    remove d3 logic

--- client/src/components/pages/stack/StackVisualization.jsx
+++ client/src/components/pages/stack/StackVisualization.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import * as d3 from 'd3';
-// import { useD3 } from '../../../hooks/useD3';
 
 class StackVisualization extends React.Component {
 
@@ -10,20 +8,14 @@ class StackVisualization extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.refSVG);
-    d3.select(this.refSVG.current)
-      .append('rect')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', 200)
-      .attr('height', 300)
-      .attr('fill', 'orange');
   }
 
   render() {
     return (
       <div>
-        <svg ref={this.refSVG} />
+        <svg ref={this.refSVG} style={{
+          width: '100%'
+        }}/>
       </div>
     );
   }
